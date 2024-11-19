a93a9d199fcec73c157f5e96f561960617a29043
Mirko P <mirkopesenti@valeo.it>
2024-11-19 21:04:06 +0100

    Consumable model

--- module/documents/items/consumable-data-model.mjs
+++ module/documents/items/consumable-data-model.mjs
@@ -1,5 +1,6 @@
 /**
- * @property {string} type.value
+ * @property {string} summary
+ * @property {string} type
  * @property {string} description
  * @property {number} IPCost.value
  * @property {boolean} isFavorite.value
@@ -9,6 +10,7 @@ export class ConsumableDataModel extends foundry.abstract.TypeDataModel {
 	static defineSchema() {
 		const { SchemaField, StringField, HTMLField, BooleanField, NumberField } = foundry.data.fields;
 		return {
+			summary: new SchemaField({ value: new StringField() }),
 			type: new SchemaField({ value: new StringField() }),
 			description: new HTMLField(),
 			IPCost: new SchemaField({ value: new NumberField({ initial: 1, min: 0, integer: true, nullable: false }) }),
